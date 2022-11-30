828745b2f20d98905e589ac63444717ac7b9a3c6
Scott Peat <scottpeatweb@gmail.com>
2022-11-30 20:24:09 +0000

    Working on AuthContext.js

--- web/contexts/AuthContext.js
+++ web/contexts/AuthContext.js
@@ -5,3 +5,39 @@ import { checkIsInitialized, IS_INITIALIZED } from '../flow/scripts';
 export const AuthContext = createContext({});
 
 export const useAuth = () => useContext(AuthContext);
+
+export default function AuthProvider({ children }) {
+  // Create a state variable to keep track of the currentUser
+  const [currentUser, setUser] = useState({
+    loggedIn: false,
+    addr: undefined,
+  });
+  // Create a state variable to represent if a user's account
+  // has been initialized or not
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  // Use FCL to subscribe to changes in the user (login, logout, etc)
+  // Tell FCL to call `setUser` and update our state variables
+  // if anything changes
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
+
+  // If currentUser is set, i.e. user is logged in
+  // check whether their account is initialized or not
+  useEffect(() => {
+    if (currentUser.addr) {
+      checkInit();
+    }
+  }, [currentUser]);
+
+  // Helper function to log the user out of the dApp
+  const logOut = async () => {
+    fcl.unauthenticate();
+    setUser({ loggedIn: false, addr: undefined });
+  };
+
+  // Helper function to log the user in to the dApp
+  // p.s. this feels even easier than RainbowKit, eh?
+  const logIn = () => {
+    fcl.logIn();
+  };
+}
