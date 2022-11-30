0fe69a479e3291ca4c2194b61b73b0800cd89db3
Scott Peat <scottpeatweb@gmail.com>
2022-11-30 21:23:13 +0000

    Building the homepage

--- web/flow/scripts.js
+++ web/flow/scripts.js
@@ -16,3 +16,31 @@ pub fun main(account: Address): Bool {
     return capability.check()
 }
 `;
+
+export async function getAllDomainInfos() {
+  return fcl.query({
+    cadence: GET_ALL_DOMAIN_INFOS,
+  });
+}
+
+const GET_ALL_DOMAIN_INFOS = `
+import Domains from 0xDomains
+
+pub fun main(): [Domains.DomainInfo] {
+    let allOwners = Domains.getAllOwners()
+    let infos: [Domains.DomainInfo] = []
+
+    for nameHash in allOwners.keys {
+        let publicCap = getAccount(allOwners[nameHash]!).getCapability<&Domains.Collection{Domains.CollectionPublic}>(Domains.DomainsPublicPath)
+        let collection = publicCap.borrow()!
+        let id = Domains.nameHashToIDs[nameHash]
+        if id != nil {
+            let domain = collection.borrowDomain(id: id!)
+            let domainInfo = domain.getInfo()
+            infos.append(domainInfo)
+        }
+    }
+
+    return infos
+}
+`;
