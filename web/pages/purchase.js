130df3120aa0622456f6e42562d738abb61968f7
Scott Peat <scottpeatweb@gmail.com>
2022-12-11 10:16:28 +0000

    Added the styled components code to transactions.js

--- web/pages/purchase.js
+++ web/pages/purchase.js
@@ -9,3 +9,35 @@ import styled from 'styled-components';
 
 // Maintain a constant for seconds per year
 const SECONDS_PER_YEAR = 365 * 24 * 60 * 60;
+
+const Container = styled.div`
+  background-color: #171923;
+  min-height: 100vh;
+`;
+
+const Main = styled.main`
+  color: white;
+  padding: 0 4em;
+  display: flex;
+  gap: 2em;
+  flex-direction: column;
+  width: 30%;
+  margin: auto;
+  align-items: center;
+
+  & button {
+    width: fit-content;
+  }
+`;
+
+const InputGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+
+  & input {
+    padding: 0.2em;
+    border-radius: 0.5em;
+    border-width: 0;
+  }
+`;
