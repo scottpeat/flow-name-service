e3bdd0a22c2f8c285be0d592fccb21c69899a921
Scott Peat <scottpeatweb@gmail.com>
2022-12-11 10:03:16 +0000

    Aded RegisterDomain function to transaction.js of flow

--- web/flow/transactions.js
+++ web/flow/transactions.js
@@ -22,3 +22,34 @@ transaction() {
     }
 }
 `;
+
+export async function registerDomain(name, duration) {
+  return fcl.mutate({
+    cadence: REGISTER_DOMAIN,
+    args: (arg, t) => [arg(name, t.String), arg(duration, t.UFix64)],
+    payer: fcl.authz,
+    proposer: fcl.authz,
+    authorizations: [fcl.authz],
+    limit: 1000,
+  });
+}
+
+const REGISTER_DOMAIN = `
+import Domains from 0xDomains
+import FungibleToken from 0xFungibleToken
+import NonFungibleToken from 0xNonFungibleToken
+
+transaction(name: String, duration: UFix64) {
+    let nftReceiverCap: Capability<&{NonFungibleToken.Receiver}>
+    let vault: @FungibleToken.Vault
+    prepare(account: AuthAccount) {
+        self.nftReceiverCap = account.getCapability<&{NonFungibleToken.Receiver}>(Domains.DomainsPublicPath)
+        let vaultRef = account.borrow<&FungibleToken.Vault>(from: /storage/flowTokenVault) ?? panic("Could not borrow Flow token vault reference")
+        let rentCost = Domains.getRentCost(name: name, duration: duration)
+        self.vault <- vaultRef.withdraw(amount: rentCost)
+    }
+    execute {
+        Domains.registerDomain(name: name, duration: duration, feeTokens: <- self.vault, receiver: self.nftReceiverCap)
+    }
+}
+`;
