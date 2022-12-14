ba034f58db06dec2672825574b368745c34dc00c
Scott Peat <scottpeatweb@gmail.com>
2022-12-14 08:18:24 +0000

    Started work on the Manage Page for managing all the domains a user has

--- web/pages/manage/index.js
+++ web/pages/manage/index.js
@@ -7,3 +7,18 @@ import { useAuth } from '../../contexts/AuthContext';
 import { getMyDomainInfos } from '../../flow/scripts';
 import { initializeAccount } from '../../flow/transactions';
 import styled from 'styled-components';
+
+export default function Home() {
+  const { currentUser, isInitialized, checkInit } = useAuth();
+  const [domainInfos, setDomainInfos] = useState([]);
+
+  async function initialize() {
+    try {
+      const txId = await initializeAccount();
+      await fcl.tx(txId).onceSealed();
+      await checkInit();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
