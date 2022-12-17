4d04a3abee6e8ee40a1a537f5282bab6ce2fc28f
Scott Peat <scottpeatweb@gmail.com>
2022-12-17 20:36:48 +0000

    Completed the manage domain page

--- web/flow/transactions.js
+++ web/flow/transactions.js
@@ -53,3 +53,37 @@ transaction(name: String, duration: UFix64) {
     }
 }
 `;
+
+export async function updateBioForDomain(nameHash, bio) {
+  return fcl.mutate({
+    cadence: UPDATE_BIO_FOR_DOMAIN,
+    args: (arg, t) => [arg(nameHash, t.String), arg(bio, t.String)],
+    payer: fcl.authz,
+    proposer: fcl.authz,
+    authorizations: [fcl.authz],
+    limit: 1000,
+  });
+}
+
+const UPDATE_BIO_FOR_DOMAIN = `
+import Domains from 0xDomains
+
+transaction(nameHash: String, bio: String) {
+    var domain: &{Domains.DomainPrivate}
+    prepare(account: AuthAccount) {
+        var domain: &{Domains.DomainPrivate}? = nil
+        let collectionPvt = account.borrow<&{Domains.CollectionPrivate}>(from: Domains.DomainsStoragePath) ?? panic("Could not load collection private")
+
+        let id = Domains.nameHashToIDs[nameHash]
+        if id == nil {
+            panic("Could not find domain")
+        }
+
+        domain = collectionPvt.borrowDomainPrivate(id: id!)
+        self.domain = domain!
+    }
+    execute {
+        self.domain.setBio(bio: bio)
+    }
+}
+`;
