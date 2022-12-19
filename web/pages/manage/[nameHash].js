8f129bbac73aa18a926305d799ff1b105e7c2120
Scott Peat <scottpeatweb@gmail.com>
2022-12-19 18:51:26 +0000

    Finished  [namehash].js

--- web/pages/manage/[nameHash].js
+++ web/pages/manage/[nameHash].js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import * as fcl from '@onflow/fcl';
 import Head from 'next/head';
-import NavBar from '../../components/NavBar';
+import Navbar from '../../components/Navbar';
 import { getDomainInfoByNameHash, getRentCost } from '../../flow/scripts';
 import styled from 'styled-components';
 import {
@@ -14,3 +14,230 @@ import {
 
 // constant representing seconds per year
 const SECONDS_PER_YEAR = 365 * 24 * 60 * 60;
+
+export default function ManageDomain() {
+  // Use AuthContext to gather data for current user
+  const { currentUser, isInitialized } = useAuth();
+
+  // Next Router to get access to `nameHash` query parameter
+  const router = useRouter();
+  // State variable to store the DomainInfo
+  const [domainInfo, setDomainInfo] = useState();
+  // State variable to store the bio given by user
+  const [bio, setBio] = useState('');
+  // State variable to store the address given by user
+  const [linkedAddr, setLinkedAddr] = useState('');
+  // State variable to store how many years to renew for
+  const [renewFor, setRenewFor] = useState(1);
+  // Loading state
+  const [loading, setLoading] = useState(false);
+  // State variable to store cost of renewal
+  const [cost, setCost] = useState(0.0);
+
+  // Function to load the domain info
+  async function loadDomainInfo() {
+    try {
+      const info = await getDomainInfoByNameHash(
+        currentUser.addr,
+        router.query.nameHash
+      );
+      console.log(info);
+      setDomainInfo(info);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Function which updates the bio transaction
+  async function updateBio() {
+    try {
+      setLoading(true);
+      const txId = await updateBioForDomain(router.query.nameHash, bio);
+      await fcl.tx(txId).onceSealed();
+      await loadDomainInfo();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  // Function which updates the address transaction
+  async function updateAddress() {
+    try {
+      setLoading(true);
+      const txId = await updateAddressForDomain(
+        router.query.nameHash,
+        linkedAddr
+      );
+      await fcl.tx(txId).onceSealed();
+      await loadDomainInfo();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  // Function which runs the renewal transaction
+  async function renew() {
+    try {
+      setLoading(true);
+      if (renewFor <= 0)
+        throw new Error('Must be renewing for at least one year');
+      const duration = (renewFor * SECONDS_PER_YEAR).toFixed(1).toString();
+      const txId = await renewDomain(
+        domainInfo.name.replace('.fns', ''),
+        duration
+      );
+      await fcl.tx(txId).onceSealed();
+      await loadDomainInfo();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  // Function which calculates cost of renewal
+  async function getCost() {
+    if (
+      domainInfo &&
+      domainInfo.name.replace('.fns', '').length > 0 &&
+      renewFor > 0
+    ) {
+      const duration = (renewFor * SECONDS_PER_YEAR).toFixed(1).toString();
+      const c = await getRentCost(
+        domainInfo.name.replace('.fns', ''),
+        duration
+      );
+      setCost(c);
+    }
+  }
+
+  // Load domain info if user is initialized and page is loaded
+  useEffect(() => {
+    if (router && router.query && isInitialized) {
+      loadDomainInfo();
+    }
+  }, [router]);
+
+  // Calculate cost everytime domainInfo or duration changes
+  useEffect(() => {
+    getCost();
+  }, [domainInfo, renewFor]);
+
+  if (!domainInfo) return null;
+
+  return (
+    <Container>
+      <Head>
+        <title>Flow Name Service - Manage Domain</title>
+        <meta name="description" content="Flow Name Service" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <Main>
+        <div>
+          <h1>{domainInfo.name}</h1>
+          <p>ID: {domainInfo.id}</p>
+          <p>Owner: {domainInfo.owner}</p>
+          <p>
+            Created At:{' '}
+            {new Date(
+              parseInt(domainInfo.createdAt) * 1000
+            ).toLocaleDateString()}
+          </p>
+          <p>
+            Expires At:{' '}
+            {new Date(
+              parseInt(domainInfo.expiresAt) * 1000
+            ).toLocaleDateString()}
+          </p>
+          <hr />
+          <p>Bio: {domainInfo.bio ? domainInfo.bio : 'Not Set'}</p>
+          <p>Address: {domainInfo.address ? domainInfo.address : 'Not Set'}</p>
+        </div>
+
+        <div>
+          <h1>Update</h1>
+          <inputGroup>
+            <span>Update Bio: </span>
+            <input
+              type="text"
+              placeholder="Lorem ipsum..."
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+            />
+            <button onClick={updateBio} disabled={loading}>
+              Update
+            </button>
+          </inputGroup>
+
+          <br />
+
+          <inputGroup>
+            <span>Update Address: </span>
+            <input
+              type="text"
+              placeholder="0xabcdefgh"
+              value={linkedAddr}
+              onChange={(e) => setLinkedAddr(e.target.value)}
+            />
+            <button onClick={updateAddress} disabled={loading}>
+              Update
+            </button>
+          </inputGroup>
+
+          <h1>Renew</h1>
+          <inputGroup>
+            <input
+              type="number"
+              placeholder="1"
+              value={renewFor}
+              onChange={(e) => setRenewFor(e.target.value)}
+            />
+            <span> years</span>
+            <button onClick={renew} disabled={loading}>
+              Renew Domain
+            </button>
+          </inputGroup>
+          <p>Cost: {cost} FLOW</p>
+          {loading && <p>Loading...</p>}
+        </div>
+      </Main>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: #171923;
+  min-height: 100vh;
+`;
+
+const Main = styled.main`
+  color: white;
+  padding: 0 4em;
+  display: flex;
+  justify-content: center;
+  gap: 12em;
+
+  & button {
+    windth: fit-content;
+  }
+`;
+
+const inputGroup = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 0.5em;
+  justify-content: center;
+
+  & input {
+    padding: 0.2em;
+    border-radius: 0.5em;
+    border-width: 0;
+  }
+`;
